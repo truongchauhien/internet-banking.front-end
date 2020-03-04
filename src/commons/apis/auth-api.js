a623e3a44f37b28feed6ee0bb4b7fe80e00a04ec
Truong Chau Hien <truongchauhien@gmail.com>
2020-03-04 17:04:38 +0700

    Add login api.

---
+++ src/commons/apis/auth-api.js
@@ -0,0 +1,18 @@
+import request from "./commons/api-request"
+
+/**
+ * 
+ * @param {object} payload 
+ * @param {string} payload.userName
+ * @param {string} payload.password
+ */
+const fetchLogin = (payload) => {
+    return request({
+        method: 'POST',
+        resource: 'auth/login',
+        useAccessToken: false,
+        body: payload
+    });
+}
+
+export { fetchLogin };
