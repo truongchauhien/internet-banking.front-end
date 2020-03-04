f0823359719061135c478b75b7488312180b68a1
Truong Chau Hien <truongchauhien@gmail.com>
2020-03-04 17:06:33 +0700

    Add root reducer.

---
+++ src/root-reducer.js
@@ -0,0 +1,10 @@
+import { combineReducers } from "redux";
+import loginReducer from "./modules/login/login-reducer";
+import customerReducer from "./modules/customer/customer-reducer";
+
+const rootReducer = combineReducers({
+    login: loginReducer,
+    // customer: customerReducer
+});
+
+export default rootReducer;
