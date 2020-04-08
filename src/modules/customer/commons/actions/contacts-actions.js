c7c412b754919f09bba44e81652dd26946bc0be8
truongchauhien <truongchauhien@gmail.com>
2020-04-08 18:36:45 +0700

    Add actions for contacts.

---
+++ src/modules/customer/commons/actions/contacts-actions.js
@@ -0,0 +1,81 @@
+export const FECTH_CONTACTS_REQUEST = 'FECTH_CONTACTS_REQUEST';
+export const FECTH_CONTACTS_FAILURE = 'FETCH_CONTACTS_FAILURE';
+export const FECTH_CONTACTS_SUCCESS = 'FETCH_CONTACTS_SUCCESS';
+
+export const fetchContactsRequest = (payload) => {
+    return {
+        type: FECTH_CONTACTS_REQUEST,
+        payload
+    };
+};
+
+export const fetchContactsFailure = (payload) => {
+    return {
+        type: FECTH_CONTACTS_FAILURE,
+        payload
+    };
+};
+
+export const fetchContactsSuccess = (payload) => {
+    return {
+        type: FECTH_CONTACTS_SUCCESS,
+        payload
+    };
+};
+
+export const DELETE_CONTACT_REQUEST = 'DELETE_CONTACT_REQUEST';
+export const DELETE_CONTACT_FAILURE = 'DELETE_CONTACT_FAILURE';
+export const DELETE_CONTACT_SUCCESS = 'DELETE_CONTACT_SUCCESS';
+
+export const deleteContactRequest = (payload) => ({
+    type: DELETE_CONTACT_REQUEST,
+    payload
+});
+
+export const deleteContactFailure = (payload) => ({
+    type: DELETE_CONTACT_FAILURE,
+    payload
+});
+
+export const deleteContactSuccess = (payload) => ({
+    type: DELETE_CONTACT_SUCCESS,
+    payload
+});
+
+export const PATCH_CONTACT_REQUEST = 'PATCH_CONTACT_REQUEST'
+export const PATCH_CONTACT_FAILURE = 'PATCH_CONTACT_FAILURE'
+export const PATCH_CONTACT_SUCCESS = 'PATCH_CONTACT_SUCCESS'
+
+export const patchContactRequest = (payload) =>({
+    type: PATCH_CONTACT_REQUEST,
+    payload
+});
+
+export const patchContactFailure = (payload) => ({
+    type: PATCH_CONTACT_FAILURE,
+    payload
+});
+
+export const patchContactSuccess = (payload) => ({
+    type: PATCH_CONTACT_SUCCESS,
+    payload
+});
+
+export const CREATE_CONTACT_REQUEST = 'CREATE_CONTACT_REQUEST';
+export const CREATE_CONTACT_FAILURE = 'CREATE_CONTACT_FAILURE';
+export const CREATE_CONTACT_SUCCESS = 'CREATE_CONTACT_SUCCESS';
+
+export const createContactRequest = (payload) => ({
+    type: CREATE_CONTACT_REQUEST,
+    payload
+});
+
+export const createContactFailure = (payload) => ({
+    type: CREATE_CONTACT_FAILURE,
+    payload
+});
+
+export const createContactSuccess = (payload) => ({
+    type: CREATE_CONTACT_SUCCESS,
+    payload
+});
