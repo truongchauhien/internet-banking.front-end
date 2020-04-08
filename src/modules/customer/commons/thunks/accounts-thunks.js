20c08891b9d1734a74984f67efb873d137e514c3
truongchauhien <truongchauhien@gmail.com>
2020-04-08 20:09:58 +0700

    Add accounts thunks.

---
+++ src/modules/customer/commons/thunks/accounts-thunks.js
@@ -0,0 +1,23 @@
+import {
+    fetchAccountsRequest,
+    fetchAccountsSuccess,
+    fetchAccountsFailure
+} from '../actions/account-actions';
+import { fetchAccounts } from '../../../../commons/apis/customers/accounts-api';
+
+export const thunkedFetchAccounts = () => {
+    return async (dispatch, getState) => {
+        dispatch(fetchAccountsRequest());
+
+        try {
+            const response = await fetchAccounts();
+            if (!response.ok) {
+                return dispatch(fetchAccountsFailure());
+            }
+
+            return dispatch(fetchAccountsSuccess(response.body.accounts));
+        } catch (error) {
+            return dispatch(fetchAccountsFailure());
+        }
+    };
+};
