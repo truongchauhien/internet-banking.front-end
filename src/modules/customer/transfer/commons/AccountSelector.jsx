50b8892a1c512fe1e925187930f7efa1b4372bae
truongchauhien <truongchauhien@gmail.com>
2020-04-14 09:33:24 +0700

    Add AccountSelector component.

---
+++ src/modules/customer/transfer/commons/AccountSelector.jsx
@@ -0,0 +1,44 @@
+import React, { useState } from 'react';
+import Select from '../../../../commons/components/select/select';
+
+export const AccountSelector = ({ id, accounts, defaultAccountId, onAccountSelect, showedTypes }) => {
+    const [accountId, setAccountId] = useState(defaultAccountId ?? '');
+
+    const handleOnChange = (selectedValue) => {
+        setAccountId(selectedValue);
+        onAccountSelect && onAccountSelect(selectedValue);
+    };
+
+    return (
+        <Select id={id} onChange={handleOnChange} value={accountId} placeHolder="Chọn tài khoản ...">
+            {
+                showedTypes && showedTypes.includes('CURRENT') &&
+                <Select.Group
+                    label='Tài khoản thanh toán'
+                    options={
+                        accounts && accounts.filter(account => account.accountType === 'CURRENT').map(account => ({
+                            value: account.id,
+                            label: account.accountNumber
+                        }))
+                        || []
+                    }
+                />
+            }
+            {
+                showedTypes && showedTypes.includes('DEPOSIT') &&
+                <Select.Group
+                    label='Tài khoản tiết kiệm'
+                    options={
+                        accounts && accounts.filter(account => account.accountType === 'DEPOSIT').map(account => ({
+                            value: account.id,
+                            label: account.accountNumber
+                        }))
+                        || []
+                    }
+                />
+            }
+        </Select>
+    );
+};
+
+export default AccountSelector;
