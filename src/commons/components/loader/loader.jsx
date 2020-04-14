30ce4c74f0dad7148c9f77c167b06bfb64f5d5ab
truongchauhien <truongchauhien@gmail.com>
2020-04-14 08:06:52 +0700

    Add loader component.

---
+++ src/commons/components/loader/loader.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from './loader.scss';
+
+export const Loader = (props) => {
+    return (
+        <div className={styles.loader} />
+    );
+};
+
+export default Loader;
