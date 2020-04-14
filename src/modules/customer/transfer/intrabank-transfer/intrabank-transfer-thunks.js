4d87147a0d4c2648c46823467aadc3a81fddff39
truongchauhien <truongchauhien@gmail.com>
2020-04-14 09:35:56 +0700

    Add intrabank transfer thunks.

---
+++ src/modules/customer/transfer/intrabank-transfer/intrabank-transfer-thunks.js
@@ -0,0 +1,50 @@
+import {
+    createIntrabankTransferRequest, createIntrabankTransferFailure, createIntrabankTransferSuccess,
+    confirmIntrabankTransferRequest, confirmIntrabankTransferFailure, confirmIntrabankTransferSuccess
+} from './intrabank-transfer-actions';
+import {
+    createIntrabankTransfer, confirmIntrabankTransfer
+} from '../../../../commons/apis/customers/transfer-api';
+
+/**
+ * 
+ * @param {object} payload
+ * @param {string} payload.fromAccountNumber
+ * @param {string} payload.toAccountNumber
+ * @param {number} payload.amount
+ * @param {string} payload.message
+ * @param {string} payload.whoPayFee
+ */
+export const thunkedCreateIntrabankTransfer = (payload) => {
+    return async (dispatch, getState) => {
+        dispatch(createIntrabankTransferRequest(payload));
+        try {
+            const response = await createIntrabankTransfer(payload);
+            if (!response.ok) return dispatch(createIntrabankTransferFailure());
+            return dispatch(createIntrabankTransferSuccess(response.body.transfer));
+        } catch {
+            return dispatch(createIntrabankTransferFailure());
+        }
+    };
+};
+
+/**
+ * 
+ * @param {object} payload
+ * @param {string} payload.otp
+ * @param {number} payload.transferId
+ */
+export const thunkedConfirmIntrabankTransfer = (payload) => {
+    return async (dispatch, getState) => {
+        dispatch(confirmIntrabankTransferRequest());
+        try {
+            const response = await confirmIntrabankTransfer(payload);
+            if (!response.ok) {
+                return dispatch(confirmIntrabankTransferFailure(response.body))
+            }
+            return dispatch(confirmIntrabankTransferSuccess());
+        } catch {
+            return dispatch(confirmIntrabankTransferFailure());
+        }
+    };
+};
