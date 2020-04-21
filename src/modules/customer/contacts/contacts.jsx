23087afb62b482c8d758c70bbe30cfc283c34a3e
truongchauhien <truongchauhien@gmail.com>
2020-04-21 17:22:25 +0700

    Refactoring contacts component.

--- src/modules/customer/contacts/contacts.jsx
+++ src/modules/customer/contacts/contacts.jsx
@@ -1,188 +1,97 @@
-import React, { useEffect, useState, useReducer } from 'react';
+import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import _ from 'lodash';
 import { thunkedFetchContacts, thunkedDeleteContact, thunkedPatchContact, thunkedCreateContact } from './contacts-thunks';
+import { thunkedFetchLinkedBanks } from '../linked-banks/thunks';
+import {
+    contactCreationModalOpenStatusChange,
+    contactModificationInit,
+    contactModificationModalOpenStatusChange
+} from './contacts-actions';
+import ContactCreationModal from './contact-creation-modal';
+import ContactModificationModal from './contact-modification-modal';
 import styles from './contacts.scss';
-
-const initState = {
-    newContact: {
-        name: '',
-        accountNumber: ''
-    },
-    editingContact: {
-        id: -1
-    }
-};
-
-const EDIT_EXIST_CONTACT_$CLEAR = 'EDIT_EXIST_CONTACT_$CLEAR'
-const EDIT_EXIST_CONTACT_$CHANGE = 'EDIT_EXIST_CONTACT_$CHANGE';
-const EDIT_NEW_CONTACT_$CHANGE = 'EDIT_NEW_CONTACT_$CHANGE';
-const EDIT_NEW_CONTACT_$CLEAR = 'EDIT_NEW_CONTACT_$CLEAR';
-
-const reducer = (state, action) => {
-    switch (action.type) {
-        case EDIT_EXIST_CONTACT_$CLEAR:
-            return Object.assign({}, state, {
-                editingContact: {
-                    id: -1
-                }
-            });
-        case EDIT_EXIST_CONTACT_$CHANGE:
-            return _.merge({}, state, {
-                editingContact: {
-                    ...action.payload
-                }
-            });
-        case EDIT_NEW_CONTACT_$CHANGE:
-            return _.merge({}, state, {
-                newContact: { ...action.payload }
-            });
-        case EDIT_NEW_CONTACT_$CLEAR:
-            return _.merge({}, state, {
-                newContact: {
-                    name: '',
-                    accountNumber: ''
-                }
-            });
-        default:
-            return state;
-    }
-};
 
 export const Contacts = (props) => {
     const reduxDispatch = useDispatch();
-    const contacts = useSelector(state => state.customer.contacts.byId);
-    const contactIds = useSelector(state => state.customer.contacts.allIds)
-
-    const [state, localDispatch] = useReducer(reducer, initState);
+    const { byId: contacts, allIds: contactAllIds } = useSelector(state => state.customer.contacts);
+    const { byId: linkedBanks } = useSelector(state => state.customer.linkedBanks);
 
     useEffect(() => {
         reduxDispatch(thunkedFetchContacts());
+        reduxDispatch(thunkedFetchLinkedBanks());
     }, []);
 
-    const handleDeleteClick = (id) => {
-        reduxDispatch(thunkedDeleteContact({ id: id }));
+    const handleShowContactCreationModalButtonClick = () => {
+        reduxDispatch(contactCreationModalOpenStatusChange(true));
     };
 
-    const handleEditingInputChange = ({ value, type, contactId }) => {
-        localDispatch({
-            type: EDIT_EXIST_CONTACT_$CHANGE,
-            payload: {
-                id: contactId,
-                [type]: value
-            }
-        });
+    const handleSubmitNewContact = (newContact) => {
+        reduxDispatch(thunkedCreateContact(newContact));
     };
 
-    const submitEditingChange = () => {
-        const { id: contactId, ...payload } = state.editingContact;
-        reduxDispatch(thunkedPatchContact({
-            contactId: contactId,
-            payload: payload
-        }));
-
-        localDispatch({ type: EDIT_EXIST_CONTACT_$CLEAR });
+    const handleEditContactButtonClick = (contactId) => {
+        reduxDispatch(contactModificationInit(contacts[contactId]));
+        reduxDispatch(contactModificationModalOpenStatusChange(true));
     };
 
-    const handleEditingInputEnterKeyPress = () => {
-        if (state.editingContact.id === -1) return;
-        submitEditingChange();
+    const handleDeleteContactButtonClick = (contactId) => {
+        reduxDispatch(thunkedDeleteContact({ id: contactId }));
     };
 
-    const handleEditingInputBlur = () => {
-        if (state.editingContact.id === -1) return;
-        submitEditingChange();
-    };
-
-    const handleNewContactInputChange = ({ type, value }) => {
-        localDispatch({
-            type: EDIT_NEW_CONTACT_$CHANGE,
-            payload: { [type]: value }
-        });
-    };
-
-    const handleNewContactClick = () => {
-        reduxDispatch(thunkedCreateContact(state.newContact));
-        localDispatch({ type: EDIT_NEW_CONTACT_$CLEAR });
-    };
+    const handleSubmitModificatedContact = (modificatedContact) => {
+        reduxDispatch(thunkedPatchContact(modificatedContact));
+    }
 
     return (
-        <div className={styles.contactTable}>
-            <table>
-                <thead>
-                    <tr>
-                        <th>Tên liên hệ</th>
-                        <th>Số tài khoản</th>
-                        <th>Tùy chọn</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {contactIds && contactIds.map((id, index) => {
-                        const contact = contacts[id];
-                        return (
-                            <tr key={contact.id}>
-                                <td>
-                                    <input defaultValue={contact.name}
-                                        onChange={e => {
-                                            const newValue = e.target.value;
-                                            handleEditingInputChange({
-                                                value: newValue,
-                                                type: 'name',
-                                                contactId: contact.id
-                                            });
-                                        }}
-                                        onKeyPress={(e) => {
-                                            if (e.key === 'Enter') {
-                                                handleEditingInputEnterKeyPress();
-                                            }
-                                        }}
-                                        onBlur={(e) => {
-                                            handleEditingInputBlur();
-                                        }}
-                                    />
-                                </td>
-                                <td>
-                                    <input defaultValue={contact.accountNumber}
-                                        onChange={e => {
-                                            const newValue = e.target.value;
-                                            handleEditingInputChange({
-                                                value: newValue,
-                                                type: 'accountNumber',
-                                                contactId: contact.id
-                                            });
-                                        }}
-                                        onKeyPress={(e) => {
-                                            if (e.key === 'Enter') {
-                                                handleEditingInputEnterKeyPress();
-                                            }
-                                        }}
-                                        onBlur={(e) => {
-                                            handleEditingInputBlur();
-                                        }}
-                                    />
-                                </td>
-                                <td>
-                                    <button onClick={(e) => { e.preventDefault(); handleDeleteClick(contact.id) }}>Xóa</button>
-                                </td>
-                            </tr>
-                        );
-                    })}
-                </tbody>
-                <tfoot>
-                    <tr>
-                        <td>
-                            <input value={state.newContact.name} onChange={e => handleNewContactInputChange({ type: 'name', value: e.target.value })} />
-                        </td>
-                        <td>
-                            <input value={state.newContact.accountNumber} onChange={e => handleNewContactInputChange({ type: 'accountNumber', value: e.target.value })} />
-                        </td>
-                        <td>
-                            <button onClick={handleNewContactClick}>Thêm</button>
-                        </td>
-                    </tr>
-                </tfoot>
-            </table>
-        </div>
+        <React.Fragment>
+            <div>
+                <button onClick={handleShowContactCreationModalButtonClick}>Thêm liên hệ ...</button>
+                <ContactCreationModal onSubmit={handleSubmitNewContact} />
+            </div>
+            <div className={styles.contactTable}>
+                <table>
+                    <thead>
+                        <tr>
+                            <th>Tên liên hệ</th>
+                            <th>Số tài khoản</th>
+                            <th>Tùy chọn</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {contactAllIds && contactAllIds.map((id, index) => {
+                            const contact = contacts[id];
+                            return (
+                                <React.Fragment key={contact.id}>
+                                    <tr>
+                                        <td rowSpan='2'>
+                                            {contact.name}
+                                        </td>
+                                        <td>
+                                            {contact.accountNumber}
+                                        </td>
+                                        <td rowSpan='2'>
+                                            <button onClick={() => handleEditContactButtonClick(contact.id)}>Thay đổi</button>
+                                            <button onClick={() => handleDeleteContactButtonClick(contact.id)}>Xóa</button>
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td>{contact.bankId ?
+                                            (linkedBanks[contact.bankId] ?
+                                                linkedBanks[contact.bankId].name :
+                                                'Ngân hàng không còn được hỗ trợ')
+                                            :
+                                            'Tài khoản nội bộ'}
+                                        </td>
+                                    </tr>
+                                </React.Fragment>
+                            );
+                        })}
+                    </tbody>
+                </table>
+            </div>
+            <ContactModificationModal onSubmit={handleSubmitModificatedContact} />
+        </React.Fragment>
     );
 };
 
