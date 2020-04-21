30bfed893efd5d266cb61c16d4c57cfc5a407eff
truongchauhien <truongchauhien@gmail.com>
2020-04-21 17:24:04 +0700

    Add customer slice in root reducer.

--- src/root-reducer.js
+++ src/root-reducer.js
@@ -1,6 +1,6 @@
 import { combineReducers } from "redux";
 import loginReducer from "./modules/login/login-reducer";
-import customerReducer from "./modules/customer/customer-reducer";
+import customerReducer from "./modules/customer/reducer";
 
 const rootReducer = combineReducers({
     login: loginReducer,
