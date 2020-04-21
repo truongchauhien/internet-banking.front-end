93962f8fc228227ac549b04713255c46fab9a9a5
truongchauhien <truongchauhien@gmail.com>
2020-04-21 17:21:10 +0700

    Add payload for patchContactSuccess.

--- src/modules/customer/contacts/contacts-thunks.js
+++ src/modules/customer/contacts/contacts-thunks.js
@@ -47,13 +47,16 @@ export const thunkedDeleteContact = ({ id }) => {
     };
 };
 
-export const thunkedPatchContact = ({ contactId, payload }) => {
+export const thunkedPatchContact = ({ id, ...payload }) => {
     return async (dispatch, getState) => {
         dispatch(patchContactRequest());
         try {
-            const response = await patchContact({ contactId, ...payload });
+            const response = await patchContact({ id, ...payload });
             if (!response.ok) return dispatch(patchContactFailure());
-            return dispatch(patchContactSuccess());
+            return dispatch(patchContactSuccess({
+                id,
+                ...payload
+            }));
         } catch {
             return dispatch(patchContactFailure());
         }
