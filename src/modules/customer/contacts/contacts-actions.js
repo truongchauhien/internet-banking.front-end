9e9ef84fc2497b4bd3e0156446ec8ea81f133d06
truongchauhien <truongchauhien@gmail.com>
2020-04-21 17:19:44 +0700

    Add new contact actions.

--- src/modules/customer/contacts/contacts-actions.js
+++ src/modules/customer/contacts/contacts-actions.js
@@ -46,7 +46,7 @@ export const PATCH_CONTACT_REQUEST = 'PATCH_CONTACT_REQUEST'
 export const PATCH_CONTACT_FAILURE = 'PATCH_CONTACT_FAILURE'
 export const PATCH_CONTACT_SUCCESS = 'PATCH_CONTACT_SUCCESS'
 
-export const patchContactRequest = (payload) =>({
+export const patchContactRequest = (payload) => ({
     type: PATCH_CONTACT_REQUEST,
     payload
 });
@@ -77,5 +77,63 @@ export const createContactFailure = (payload) => ({
 
 export const createContactSuccess = (payload) => ({
     type: CREATE_CONTACT_SUCCESS,
+    payload
+});
+
+export const CONTACT_CREATION_INIT = 'CONTACT_CREATION_INIT';
+export const contactCreationInit = (payload) => ({
+    type: CONTACT_CREATION_INIT,
+    payload
+});
+
+export const CONTACT_CREATION_INPUT_CHANGE = 'CONTACT_CREATION_INPUT_CHANGE';
+
+/**
+ * 
+ * @param {object} payload
+ * @param {string} payload.field
+ * @param {string} payload.value
+ */
+export const contactCreationInputChange = (payload) => ({
+    type: CONTACT_CREATION_INPUT_CHANGE,
+    payload
+});
+
+export const CONTACT_CREATION_MODAL_OPEN_STATUS_CHANGE = 'CONTACT_CREATION_MODAL__OPEN_STATUS_CHANGE';
+
+/**
+ * 
+ * @param {boolean} payload
+ */
+export const contactCreationModalOpenStatusChange = (payload) => ({
+    type: CONTACT_CREATION_MODAL_OPEN_STATUS_CHANGE,
+    payload
+});
+
+export const CONTACT_MODIFICATION_INPUT_CHANGE = 'CONTACT_MODIFICATION_INPUT_CHANGE';
+
+export const contactModificationInputChange = (payload) => ({
+    type: CONTACT_MODIFICATION_INPUT_CHANGE,
+    payload
+});
+
+export const CONTACT_MODIFICATION_MODAL_OPEN_STATUS_CHANGE = 'CONTACT_MODIFICATION_MODAL_OPEN_STATUS_CHANGE';
+export const contactModificationModalOpenStatusChange = (payload) => ({
+    type: CONTACT_MODIFICATION_MODAL_OPEN_STATUS_CHANGE,
+    payload
+});
+
+export const CONTACT_MODIFICATION_INIT = 'CONTACT_MODIFICATION_INIT';
+
+/**
+ * 
+ * @param {object} payload
+ * @param {number} payload.id,
+ * @param {string} payload.name,
+ * @param {number} payload.bankId,
+ * @param {string} payload.accountNumber
+ */
+export const contactModificationInit = (payload) => ({
+    type: CONTACT_MODIFICATION_INIT,
     payload
 });
