e1ee0f5794630ff9d2d9ab895180e36ffa5c2432
truongchauhien <truongchauhien@gmail.com>
2020-04-28 01:58:39 +0700

    Add password change thunks.

---
+++ src/modules/customer/profile/password-change/thunks.js
@@ -0,0 +1,20 @@
+import { createPasswordChangeRequest, createPasswordChangeFailure, createPasswordChangeSuccess } from "./actions";
+import { createCustomerPasswordChange } from "../../../../commons/apis/customers-api";
+
+/**
+ * 
+ * @param {object} payload
+ * @param {number} payload.customerId
+ * @param {string} payload.oldPassword
+ * @param {string} payload.newPassword
+ */
+export const thunkedCreatePasswordChange = (payload) => async (dispatch, getState) => {
+    dispatch(createPasswordChangeRequest())
+    try {
+        const response = await createCustomerPasswordChange(payload);
+        if (!response.ok) return dispatch(createPasswordChangeFailure());
+        return dispatch(createPasswordChangeSuccess());
+    } catch {
+        return dispatch(createPasswordChangeFailure());
+    }
+};
