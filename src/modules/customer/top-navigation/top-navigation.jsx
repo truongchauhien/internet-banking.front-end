be284d5c4895badbd504590e17beb173d3dce24c
truongchauhien <truongchauhien@gmail.com>
2020-04-27 15:49:34 +0700

    Add top navigation component.

---
+++ src/modules/customer/top-navigation/top-navigation.jsx
@@ -0,0 +1,31 @@
+import React, { useCallback } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { NavLink, useRouteMatch, useHistory } from 'react-router-dom';
+import Dropdown from '../../../commons/components/dropdown/dropdown';
+import { thunkedLogout } from '../../authentication/thunks';
+import styles from './top-navigation.scss';
+
+export const TopNavigation = (props) => {
+    const match = useRouteMatch();
+    const reactRouterHistory = useHistory();
+
+    const dispatch = useDispatch();
+    const { fullName } = useSelector(state => state.authentication.userData);
+
+    const handleDropdownProfileClick = useCallback(() => {
+        reactRouterHistory.push(`${match.url}/profile`);
+    }, [match]);
+
+    const handleDropdownLogoutClick = () => {
+        dispatch(thunkedLogout());
+    };
+
+    return (
+        <div className={styles.topnav}>
+            <Dropdown className={styles.userDropdown} label={fullName}>
+                <Dropdown.Item onClick={handleDropdownProfileClick}>Tài khoản</Dropdown.Item>
+                <Dropdown.Item onClick={handleDropdownLogoutClick}>Đăng xuất</Dropdown.Item>
+            </Dropdown>
+        </div>
+    );
+};
