b3c2e1940dd4b3fbb8f50ac1e562054ba469b491
truongchauhien <truongchauhien@gmail.com>
2020-04-27 15:10:10 +0700

    Add dropdown component.

---
+++ src/commons/components/dropdown/dropdown.jsx
@@ -0,0 +1,51 @@
+import React, { useState } from 'react';
+import styles from './dropdown.scss';
+
+export const Dropdown = ({ label, children, className }) => {
+    const [isOpen, setIsOpen] = useState(false);
+
+    const handleOnMouseEnter = () => {
+        setIsOpen(true);
+    };
+
+    const handleOnMouseLeave = () => {
+        setIsOpen(false);
+    };
+
+    const handleItemClick = () => {
+        setIsOpen(false);
+    };
+
+    return (
+        <div
+            className={`${styles.dropdown} ${className ? className : ''}`}
+            onMouseEnter={handleOnMouseEnter}
+            onMouseLeave={handleOnMouseLeave}
+        >
+            <button className={styles.dropdownButton}>{label}</button>
+            <div
+                className={`${styles.dropdownContent} ${!isOpen ? styles.dropdownContentHidden : ''}`}
+                onClick={handleItemClick}
+            >
+                {children}
+            </div>
+        </div>
+    );
+};
+
+Dropdown.Item = ({ onClick, children }) => {
+    const handleClick = (event) => {
+        event.preventDefault();
+        onClick && onClick();
+    };
+
+    if (typeof children === 'string') {
+        return (
+            <a onClick={handleClick} href="#">{children}</a>
+        );
+    }
+
+    return null;
+};
+
+export default Dropdown;
