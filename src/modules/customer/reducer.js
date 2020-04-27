e325a1f3d7a8befc9cc0d8469ea00ef89c3bab39
truongchauhien <truongchauhien@gmail.com>
2020-04-28 02:00:19 +0700

    Add profile slice to customer reducer.

--- src/modules/customer/reducer.js
+++ src/modules/customer/reducer.js
@@ -4,13 +4,15 @@ import contactsReducer from "./contacts/contacts-reducer";
 import transfersReducer from "./transfers/reducer";
 import linkedBanksReducer from "./linked-banks/reducer";
 import debtsReducer from "./debts/reducers";
+import profileReducer from "./profile/reducer";
 
 export const customerReducer = combineReducers({
     accounts: accountsReducer,
     contacts: contactsReducer,
     transfers: transfersReducer,
     linkedBanks: linkedBanksReducer,
-    debts: debtsReducer
+    debts: debtsReducer,
+    profile: profileReducer
 });
 
 export default customerReducer;
