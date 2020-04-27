bc5a1618ec9f2ef8a4b54ed54b8bc961df4f50d6
truongchauhien <truongchauhien@gmail.com>
2020-04-27 15:44:20 +0700

    Add received debts reducer.

---
+++ src/modules/customer/debts/received/reducer.js
@@ -0,0 +1,35 @@
+import { combineReducers } from "redux";
+import {
+    PAYDEBT_TRANSFER_CREATE_REQUEST,
+    PAYDEBT_TRANSFER_CREATE_FAILURE,
+    PAYDEBT_TRANSFER_CREATE_SUCCESS,
+    PAYDEBT_TRANSFER_CONFIRM_FAILURE,
+    PAYDEBT_TRANSFER_CONFIRM_SUCCESS
+} from "../../transfers/paydebt/actions";
+
+const initState = {
+    doingTransferForDebtId: null
+};
+
+const doingTransferForDebtIdReducer = (state = initState.doingTransferForDebtId, action) => {
+    switch (action.type) {
+        case PAYDEBT_TRANSFER_CREATE_REQUEST:
+            return action.payload.debtId;
+        case PAYDEBT_TRANSFER_CREATE_FAILURE:
+            return null;
+        case PAYDEBT_TRANSFER_CREATE_SUCCESS:
+            return action.payload.debtId;
+        case PAYDEBT_TRANSFER_CONFIRM_FAILURE:
+            return state;
+        case PAYDEBT_TRANSFER_CONFIRM_SUCCESS:
+            return null;
+        default:
+            return state;
+    }
+};
+
+export const receivedReducer = combineReducers({
+    doingTransferForDebtId: doingTransferForDebtIdReducer
+});
+
+export default receivedReducer;
