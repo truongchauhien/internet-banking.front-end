1facfcbf87a6c660e64c2dada6a283c262589c43
truongchauhien <truongchauhien@gmail.com>
2020-04-27 15:57:46 +0700

    Refactoring transfer component.

--- src/modules/customer/transfers/transfers.jsx
+++ src/modules/customer/transfers/transfers.jsx
@@ -3,21 +3,20 @@ import { useSelector, useDispatch } from 'react-redux';
 import Tabs from '../../../commons/components/tabs/tabs';
 import { thunkedFetchAccounts } from '../accounts/accounts-thunks';
 import { thunkedFetchContacts } from '../contacts/contacts-thunks';
-import IntraBankTransfer from './intrabank-transfer/intrabank-transfer';
-import styles from './transfer.scss';
+import IntraBank from './intrabank/intrabank';
+import styles from './transfers.scss';
 
-export const Transfer = (props) => {
+export const Transfers = (props) => {
     const reduxDispatch = useDispatch();
 
     useEffect(() => {
         reduxDispatch(thunkedFetchAccounts());
-        reduxDispatch(thunkedFetchContacts());
     }, []);
 
     return (
         <Tabs defaultActiveLabel='Chuyển khoản nội bộ'>
             <Tabs.Tab label='Chuyển khoản nội bộ' >
-                <IntraBankTransfer />
+                <IntraBank />
             </Tabs.Tab>
             <Tabs.Tab label='Chuyển khoản liên ngân hàng'>
                 <h1>Liên ngân hàng</h1>
@@ -29,4 +28,4 @@ export const Transfer = (props) => {
     );
 };
 
-export default Transfer;
+export default Transfers;
