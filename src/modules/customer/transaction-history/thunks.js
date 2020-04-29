00146975d8957bae2bbedfcbc92e95ef4689e8b4
truongchauhien <truongchauhien@gmail.com>
2020-04-29 15:51:05 +0700

    Add transaction history thunks.

---
+++ src/modules/customer/transaction-history/thunks.js
@@ -0,0 +1,27 @@
+import {
+    fetchTransactionsRequest,
+    fetchTransactionsFailure,
+    fetchTransactionsSuccess
+} from './actions';
+import { fetchTransactions } from '../../../commons/apis/transactions-api';
+
+/**
+ * 
+ * @param {object} payload
+ * @param {object} payload.accountId
+ * @param {object} payload.startingAfter
+ */
+export const thunkedFetchTransactions = (payload) => async (dispatch, getState) => {
+    dispatch(fetchTransactionsRequest());
+    try {
+        const response = await fetchTransactions(payload);
+        if (!response.ok) return dispatch(fetchTransactionsFailure());
+        const shouldMerge = payload.startingAfter !== null && payload.startingAfter !== undefined;
+        return dispatch(fetchTransactionsSuccess({
+            ...response.body,
+            shouldMerge
+        }));
+    } catch {
+        return dispatch(fetchTransactionsFailure());
+    }
+};
