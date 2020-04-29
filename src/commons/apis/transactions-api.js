72ba0df81bdd54856e5f229aa093b499837e312f
truongchauhien <truongchauhien@gmail.com>
2020-04-29 16:26:03 +0700

    Add fetch transactions api.

---
+++ src/commons/apis/transactions-api.js
@@ -0,0 +1,14 @@
+import request from "./commons/api-request";
+
+/**
+ * 
+ * @param {object} payload
+ * @param {number} payload.accountId
+ * @param {number} payload.startingAfter
+ */
+export const fetchTransactions = (payload) => request({
+    method: 'GET',
+    resource: '/api/transactions',
+    params: payload,
+    useAccessToken: true
+});
