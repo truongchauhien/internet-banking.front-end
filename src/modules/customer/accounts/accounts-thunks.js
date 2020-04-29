c523f049c6ddc6ffc3d7f477dc062c130183b689
truongchauhien <truongchauhien@gmail.com>
2020-04-29 16:27:15 +0700

    Prevent re-fetch accounts.

--- src/modules/customer/accounts/accounts-thunks.js
+++ src/modules/customer/accounts/accounts-thunks.js
@@ -5,12 +5,23 @@ import {
 } from './account-actions';
 import { fetchAccounts } from '../../../commons/apis/accounts-api';
 
-export const thunkedFetchAccounts = () => {
+/**
+ * 
+ * @param {object} payload
+ * @param {number} payload.customerId
+ */
+export const thunkedFetchAccounts = (payload) => {
     return async (dispatch, getState) => {
+        const accountAllIds = _.get(getState(), ['customer','accounts','allIds']);
+        if (accountAllIds.length !== 0) {
+            // Do not fetch again.
+            return;
+        }
+        
         dispatch(fetchAccountsRequest());
 
         try {
-            const response = await fetchAccounts();
+            const response = await fetchAccounts(payload);
             if (!response.ok) {
                 return dispatch(fetchAccountsFailure());
             }
