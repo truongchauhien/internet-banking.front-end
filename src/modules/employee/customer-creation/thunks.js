b9ea1e88915fda16c5087850ce524021fffd874b
truongchauhien <truongchauhien@gmail.com>
2020-05-05 18:47:43 +0700

    Add customer creation thunks.

---
+++ src/modules/employee/customer-creation/thunks.js
@@ -0,0 +1,22 @@
+import { createCustomerRequest, createCustomerFailure, createCustomerSuccess } from "./actions";
+import { createCustomer } from "../../../commons/apis/customers-api";
+
+/**
+ * 
+ * @param {object} payload
+ * @param {object} payload.userName
+ * @param {object} payload.password
+ * @param {object} payload.fullName
+ * @param {object} payload.email
+ * @param {object} payload.phone
+ */
+export const thunkedCreateCustomer = (payload) => async (dispatch, getState) => {
+    dispatch(createCustomerRequest());
+    try {
+        const response = await createCustomer(payload);
+        if (!response.ok) return dispatch(createCustomerFailure());
+        return dispatch(createCustomerSuccess(response.body.customer));
+    } catch {
+        return dispatch(createCustomerFailure());
+    }
+};
