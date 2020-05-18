dc9644cd4db51557e92bd1f43953e81d413a802d
truongchauhien <truongchauhien@gmail.com>
2020-05-18 11:19:56 +0700

    Add administrator component.

---
+++ src/modules/administrator/administrator.jsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import TopNavigation from './top-navigation/top-navigation';
+import { NavLink, useRouteMatch, Switch, Route } from 'react-router-dom';
+import SideBar from '../../commons/components/sidebar/sidebar';
+import Employees from './employees/employees';
+import Reconciliations from './reconciliations/reconciliations';
+import styles from './administrator.scss';
+
+export const Administrator = (props) => {
+    const match = useRouteMatch();
+
+    return (
+        <React.Fragment>
+            <TopNavigation />
+            <SideBar>
+                <SideBar.Menu>
+                    <SideBar.Menu.Item>
+                        <NavLink to={`${match.url}/employees`} activeClassName={styles.activeSidebarMenuItem}>
+                            Quản lý nhân viên
+                        </NavLink>
+                    </SideBar.Menu.Item>
+                    <SideBar.Menu.Item>
+                        <NavLink to={`${match.url}/reconciliations`} activeClassName={styles.activeSidebarMenuItem}>
+                            Danh sách giao dịch
+                        </NavLink>
+                    </SideBar.Menu.Item>
+                </SideBar.Menu>
+                <SideBar.Content>
+                    <Switch>
+                        <Route path={`${match.path}/employees`}>
+                            <Employees />
+                        </Route>
+                        <Route path={`${match.path}/reconciliations`}>
+                            <Reconciliations />
+                        </Route>
+                    </Switch>
+                </SideBar.Content>
+            </SideBar>
+        </React.Fragment>
+    );
+};
+
+export default Administrator;
