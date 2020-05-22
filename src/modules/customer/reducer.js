c5b7b198946a301fd657e34197ec278ce1c85af1
truongchauhien <truongchauhien@gmail.com>
2020-05-22 20:02:16 +0700

    Add fetch customer thunk.

--- src/modules/customer/reducer.js
+++ src/modules/customer/reducer.js
@@ -2,7 +2,6 @@ import { combineReducers } from "redux";
 import accountsReducer from './accounts/reducer';
 import contactsReducer from "./contacts/reducer";
 import transfersReducer from "./transfers/reducer";
-import linkedBanksReducer from "./linked-banks/reducer";
 import debtsReducer from "./debts/reducers";
 import transactionsReducer from "./transaction-history/reducer";
 import profileReducer from "./profile/reducer";
@@ -11,7 +10,6 @@ export const customerReducer = combineReducers({
     accounts: accountsReducer,
     contacts: contactsReducer,
     transfers: transfersReducer,
-    linkedBanks: linkedBanksReducer,
     debts: debtsReducer,
     transactions: transactionsReducer,
     profile: profileReducer
---
+++ src/modules/customer/thunks.js
@@ -0,0 +1,19 @@
+import { fetchCustomerRequest, fetchCustomerFailure, fetchCustomerSuccess } from "./actions";
+import { fetchCustomer } from "../../commons/apis/customers-api";
+
+/**
+ * 
+ * @param {object} payload
+ * @param {number} payload.customerId
+ */
+export const thunkedFetchCustomer = (payload) => async (dispatch,getState) => {
+    dispatch(fetchCustomerRequest());
+    try {
+        const response = await fetchCustomer(payload);
+        if (!response.ok) return dispatch(fetchCustomerFailure());
+        return dispatch(fetchCustomerSuccess(response.body));
+    } catch (err) {
+        console.log(err);
+        return dispatch(fetchCustomerFailure());
+    }
+};
